e8f94b1ceb3f4d7baf7121133a45347d5e055ba0
derwinirawan <d_erwin_irawan@yahoo.com>
2018-11-17 04:20:27 +0700

    new lines "citescore.R"

--- citescore_anl/citescore.R
+++ citescore_anl/citescore.R
@@ -1,4 +1,16 @@
-d = read.csv('scopus(2).csv')
+d = read.csv('citescore_anl/scopus_climate_allyears.csv')
 plot(d$age, d$Cited.by) 
 corel <- lm(d$Cited.by~d$age)
+print(summary(corel))
+
+d2 = read.csv('citescore_anl/scopus_paleontology_allyears.csv')
+d2$age = 2017 - d2$Year
+plot(d2$age, d2$Cited.by) 
+corel <- lm(d2$Cited.by~d2$age)
+print(summary(corel))
+
+d4 = read.csv('citescore_anl/scopus_structure_allyears.csv')
+d4$age = 2017 - d4$Year
+plot(d4$age, d3$Cited.by) 
+corel <- lm(d3$Cited.by~d3$age)
 print(summary(corel))
